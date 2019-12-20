f96330a5350dc602f028c6ac93eddf3f2a987827
unknown <tduong2049@gmail.com>
2019-12-20 14:03:25 -0500

    Changed output format

--- flight_data_display.py
+++ flight_data_display.py
@@ -2,12 +2,17 @@
 from dronekit import connect, VehicleMode, LocationGlobalRelative
 import time
 
+fileName = raw_input("Enter the file name to store flight data on: ")
+outputFile = open(fileName, "w+")
+print "\nWriting ouput to " + fileName + "\n"
+
 drone_address = "/dev/ttyS0"
 
 # Create a drone object after connecting to the address of the drone
 print "Connecting to drone on: " + drone_address
-drone = connect(drone_address, baud = 921600 wait_ready = True)
+drone = connect(drone_address, baud = 921600, wait_ready = True)
 
+'''
 # Sets the drone to GUIDED mode
 print "\nSetting drone to GUIDED mode now..."
 drone.mode = VehicleMode("GUIDED")
@@ -30,22 +35,25 @@ print("\nDrone is armed!")
 targetAltitude = 10.0
 print "\nTarget altitude of " + str(targetAltitude) + "m has been set."
 drone.simple_takeoff(targetAltitude)
+'''
 
-outputFile = open("output.txt", "w+")
-print "\nWriting ouput to 'output.txt'"
-print "Press [CTRL + C] to stop recording data and end flight."
+print "\nConnected to the drone on: " + drone_address
+print "Press [CTRL + C] to stop recording data and end flight.\n"
+iteration = 0
+startTime = time.time()
 
-# Loop for displaying the drone's lat, long, alt, pitch, yaw, and roll
-# can be broken when user presses CTRL + C
 try:
-    while True:
-        outputFile.write("\nLatitude: \t" + str(drone.location.global_relative_frame.lat))
-        outputFile.write("\nLongitude: \t" + str(drone.location.global_relative_frame.lon))
-        outputFile.write("\nAltitude: \t" + str(drone.location.global_relative_frame.alt))
-        outputFile.write("\nPitch: \t\t" + str(drone.attitude.pitch))
-        outputFile.write("\nYaw: \t\t" + str(drone.attitude.yaw))
-        outputFile.write("\nRoll: \t\t" + str(drone.attitude.roll))
-        outputFile.write("\n________________________________________")
+    while iteration < 1000:
+        outputFile.write("\n" + str(drone.location.global_relative_frame.lat) + ', ')
+        outputFile.write(str(drone.location.global_relative_frame.lon)+ ', ')
+        outputFile.write(str(drone.location.global_relative_frame.alt)+ ', ')
+        outputFile.write(str(drone.attitude.pitch) + ', ')
+        outputFile.write(str(drone.attitude.yaw) + ', ')
+        outputFile.write(str(drone.attitude.roll))
+        iteration += 1
+
+        print "Completed Iteration: " + str(iteration)
+        time.sleep(0.0025)
         
         '''
         print "\nLatitude: " + str(drone.location.global_relative_frame.lat)
@@ -58,6 +66,7 @@ try:
         '''
 except KeyboardInterrupt:
     pass
+
 
 outputFile.close()
 
--- flight_data_display_sitl.py
+++ flight_data_display_sitl.py
@@ -3,6 +3,11 @@ from dronekit import connect, VehicleMode, LocationGlobalRelative
 import dronekit_sitl
 import time
 
+fileName = raw_input("Enter the file name to store flight data on: ")
+outputFile = open(fileName, "w+")
+print "\nWriting ouput to " + fileName + "\n"
+
+
 # Start SITL and store its address in a string variable
 sitl = dronekit_sitl.start_default()
 drone_address = sitl.connection_string()
@@ -11,6 +16,7 @@ drone_address = sitl.connection_string()
 print "Connecting to drone on: " + drone_address
 drone = connect(drone_address, wait_ready = True)
 
+'''
 # Sets the drone to GUIDED mode
 print "\nSetting drone to GUIDED mode now..."
 drone.mode = VehicleMode("GUIDED")
@@ -33,22 +39,27 @@ print("\nDrone is armed!")
 targetAltitude = 10.0
 print "\nTarget altitude of " + str(targetAltitude) + "m has been set."
 drone.simple_takeoff(targetAltitude)
+'''
 
-outputFile = open("output.txt", "w+")
-print "\nWriting ouput to 'output.txt'"
-print "Press [CTRL + C] to stop recording data and end flight."
+print "\nConnected to the drone on: " + drone_address
+print "Press [CTRL + C] to stop recording data and end flight.\n"
+iteration = 0
+startTime = time.time()
 
 # Loop for displaying the drone's lat, long, alt, pitch, yaw, and roll
 # can be broken when user presses CTRL + C
 try:
-    while True:
-        outputFile.write("\nLatitude: \t" + str(drone.location.global_relative_frame.lat))
-        outputFile.write("\nLongitude: \t" + str(drone.location.global_relative_frame.lon))
-        outputFile.write("\nAltitude: \t" + str(drone.location.global_relative_frame.alt))
-        outputFile.write("\nPitch: \t\t" + str(drone.attitude.pitch))
-        outputFile.write("\nYaw: \t\t" + str(drone.attitude.yaw))
-        outputFile.write("\nRoll: \t\t" + str(drone.attitude.roll))
-        outputFile.write("\n________________________________________")
+    while iteration < 1000:
+        outputFile.write("\n" + str(drone.location.global_relative_frame.lat) + ', ')
+        outputFile.write(str(drone.location.global_relative_frame.lon)+ ', ')
+        outputFile.write(str(drone.location.global_relative_frame.alt)+ ', ')
+        outputFile.write(str(drone.attitude.pitch) + ', ')
+        outputFile.write(str(drone.attitude.yaw) + ', ')
+        outputFile.write(str(drone.attitude.roll))
+        iteration += 1
+
+        print "Completed Iteration: " + str(iteration)
+        time.sleep(0.0025)
         
         '''
         print "\nLatitude: " + str(drone.location.global_relative_frame.lat)
